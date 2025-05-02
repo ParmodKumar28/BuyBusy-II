88f1ac65c7eb81f27dc6aa498b0a08bc4b332f9e
Parmod Kumar <yparmodyadav@gmail.com>
2025-05-02 22:38:02 +0530

    Fixed Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,40 @@
-// Imports
 import { NavLink, Outlet } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutAsync, userSelector, verifyUserSignIn } from "../../Redux/Reducer/userReducer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-// Functional component for the Navbar.
 export default function Navbar() {
-    // Consuming state from userReducer using useSelector
     const { isSignIn } = useSelector(userSelector);
     const dispatch = useDispatch();
     const auth = getAuth();
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    // Side effect to check if the user is signedIn or not here from firebase
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
-                // User is signed in
                 dispatch(verifyUserSignIn({ user: user.uid, signIn: true }));
             } else {
-                // No user is signed in
                 dispatch(verifyUserSignIn({ user: null, signIn: false }));
             }
         });
 
         return () => unsubscribe();
-
     }, [auth, dispatch]);
 
-    // After login take to home page
     useEffect(() => {
         if (isSignIn) {
             navigate("/");
         }
     }, [isSignIn, navigate]);
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-    // Returning JSx
     return (
         <>
             <div className={styles.navbarContainer}>
@@ -50,9 +45,15 @@ export default function Navbar() {
                     </NavLink>
                 </div>
 
+                {/* Hamburger menu for mobile */}
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
                 {/* Right side of nav */}
-                <ul className={styles.right}>
-                    {/* Home */}
+                <ul className={`${styles.right} ${menuOpen ? styles.active : ""}`}>
                     <li>
                         <NavLink to="/" className={styles.navLink}>
                             <img src="https://cdn-icons-png.flaticon.com/128/609/609803.png" alt="Home" />
@@ -60,18 +61,14 @@ export default function Navbar() {
                         </NavLink>
                     </li>
 
-                    {/* If signed in then shows cart and my orders. */}
-                    {isSignIn &&
+                    {isSignIn && (
                         <>
-                            {/* My orders */}
                             <li>
                                 <NavLink to="/orders" className={styles.navLink}>
                                     <img src="https://cdn-icons-png.flaticon.com/128/6815/6815043.png" alt="My Orders" />
                                     <h3>My Orders</h3>
                                 </NavLink>
                             </li>
-
-                            {/* Cart */}
                             <li>
                                 <NavLink to="/cart" className={styles.navLink}>
                                     <img src="https://cdn-icons-png.flaticon.com/128/891/891407.png" alt="Cart" />
@@ -79,19 +76,28 @@ export default function Navbar() {
                                 </NavLink>
                             </li>
                         </>
-                    }
-                    {/* Showing conditionally SignIn or Logout here */}
+                    )}
                     <li>
-                        <NavLink to="/signIn" className={styles.navLink} onClick={() => isSignIn ? dispatch(logoutAsync()) : null}>
-                            <img src={isSignIn ? "https://cdn-icons-png.flaticon.com/128/1828/1828490.png" : "https://cdn-icons-png.flaticon.com/128/2574/2574000.png"} alt="SignIn" />
+                        <NavLink
+                            to="/signIn"
+                            className={styles.navLink}
+                            onClick={() => (isSignIn ? dispatch(logoutAsync()) : null)}
+                        >
+                            <img
+                                src={
+                                    isSignIn
+                                        ? "https://cdn-icons-png.flaticon.com/128/1828/1828490.png"
+                                        : "https://cdn-icons-png.flaticon.com/128/2574/2574000.png"
+                                }
+                                alt="SignIn"
+                            />
                             <h3>{isSignIn ? "Logout" : "SignIn"}</h3>
                         </NavLink>
                     </li>
                 </ul>
             </div>
 
-            {/* Showing childrens */}
             <Outlet />
         </>
-    )
+    );
 }
